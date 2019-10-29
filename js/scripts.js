f139924591265c2efa47b8fef50cba0fc32a08a3
Student <example@example.com>
2019-10-29 14:55:56 -0700

    add survey section for fun results

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   $("#transportation_survey").submit(function(event){
     event.preventDefault();
-
+    $("#results").show();
+    $("#fun-results").show();
     $("input:checkbox[name=work-transportation]:checked").each(function() {
       var transportationResults = $(this).val();
       $("#results").append(transportationResults + "<br>");
-      // $("#results").text(transportationResults);
+  });
+    $("input:checkbox[name=fun-transportation]:checked").each(function() {
+      var funTransportationResults = $(this).val();
+      $("#fun-results").append(funTransportationResults + "<br>");
     });
-
+    $("#transportation_survey").hide();
   });
 });
